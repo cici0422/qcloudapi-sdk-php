fe5bc3d3f0e1dfe4b7b74ab2213f689c5553ff81
cici0422 <96974326+cici0422@users.noreply.github.com>
2022-01-02 18:09:38 +0800

    Update demo.php

--- demo.php
+++ demo.php
@@ -1,25 +1,27 @@
 <?php
+error_reporting(E_ALL ^ E_NOTICE);
 require_once './src/QcloudApi/QcloudApi.php';
 
-$config = array('SecretId'       => getenv("QCLOUD_SECRET_ID"), //'你的secretId',需要在环境变量中设置
-                'SecretKey'      => getenv("QCLOUD_SECRET_KEY"), //'你的secretKey',需要在环境变量中设置
-                'RequestMethod'  => 'GET',
-                'DefaultRegion'  => 'gz');
+$config = array('SecretId'        => '你在腾讯云上的SecretId',
+             'SecretKey'       => '你在腾讯云上的SecretKey',
+             'RequestMethod'  => 'POST',
+             'DefaultRegion'    => 'gz');
 
-$cvm = QcloudApi::load(QcloudApi::MODULE_CVM, $config);
+$wenzhi = QcloudApi::load(QcloudApi::MODULE_WENZHI, $config);
 
-$package = array('offset' => 0, 'limit' => 3, 'SignatureMethod' =>'HmacSHA256');
+$package = array("content"=>"李亚鹏挺王菲：加油！孩儿他娘。");
 
-$a = $cvm->DescribeInstances($package);
-// $a = $cvm->generateUrl('DescribeInstances', $package);
+$a = $wenzhi->TextSentiment($package);
 
 if ($a === false) {
-    $error = $cvm->getError();
+    $error = $wenzhi->getError();
     echo "Error code:" . $error->getCode() . ".\n";
     echo "message:" . $error->getMessage() . ".\n";
     echo "ext:" . var_export($error->getExt(), true) . ".\n";
+} else {
+    var_dump($a);
 }
 
-echo "Request: " . $cvm->getLastRequest();
-echo "\nResponse: " . $cvm->getLastResponse();
+echo "\nRequest :" . $wenzhi->getLastRequest();
+echo "\nResponse :" . $wenzhi->getLastResponse();
 echo "\n";
